cc61428f56c43ee6214ef583b72e34e5c9f72cdb
DanGam96DM <35945144+DanGam96DM@users.noreply.github.com>
2020-05-06 20:46:31 -0600

    Configuracion de redux y registro completo + verificacion token

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Alert from './components/Layout/Alert';
 import './App.css';
 
-const App=()=> (
-  <Router>
-    <Fragment>
-      <Navbar/>
-      <Route exact path='/' component={Landing}/>
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-);
+//Redux
+import {Provider} from 'react-redux';
+import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App=()=> {
+  useEffect(()=>{
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path='/' component={Landing}/>
+        <section className="container">
+          <Alert/>
+          <Switch>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+)};
 
 export default App;
